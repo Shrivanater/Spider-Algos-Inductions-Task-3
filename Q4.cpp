fa9fd96b8a7e8be92853ba1826d16c138be95625
Shrivanater <83690434+Shrivanater@users.noreply.github.com>
2021-08-01 23:13:22 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,56 +1,63 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 const int N = 1e5 + 1;
 int k, n;
 
-bool check(int mid, int arr[], int k){
-        int total = 0;
+class Solution {
+public:
+    
+    bool check(long long int mid, int arr[], int m){
+        long long int total = 0;
         int sub_array = 0;
         
         for(int i = 0; i < n; i++){
+            if(arr[i] > mid){
+                return false;
+            }                
+                
             if(total + arr[i] <= mid){
                 total += arr[i];
-            }
-          
+            }               
             else{
                 sub_array++;
                 total = arr[i];
             }
         }
-        
         sub_array++;
-        return(sub_array <= k);
+        return(sub_array<=m);
     }
     
-int solve(int arr[], int k) {
-        int l = 0;
-        int r = (int)(1e14 + 1);
+    long long int solve(int arr[], int m) {
+        long long int l = 0;
+        long long int r = (long long int)(1e14 + 1);
         
         while(l < r){
-            int mid = (l + r)/2;
+            long long int mid = (l + r)/2;
             
-            if(check(mid, arr, k)){
+            if(check(mid, arr, m)){
                 r = mid;
-            }
-            
+            }                
             else{
-				l = mid + 1;
-			}             
+                l = mid + 1;
+            }              
         }
         return l;
-    }           
+    }
+};
 
 int main(){
-	int i;
-	int arr[N];
-	
-	cin >> k >> n;
-	
-	for(i = 0; i < n; i++){
-		cin >> arr[i];
-	}
-	
-	cout << solve(arr, k);
+    int i;
+    int arr[N];
+    Solution sol;
+    
+    cin >> k >> n;
+    
+    for(i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    cout << sol.solve(arr, k);
 }
